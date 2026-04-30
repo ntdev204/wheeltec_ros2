575c940513c3b80b2544bcf967004d44a08f9d0b
thien <thien.npc369@gmail.com>
2026-04-30 15:34:50 +0700

    feat(nav2): add cmd_vel remapping for navigation

--- src/navigation2-humble/nav2_bringup/launch/navigation_launch.py
+++ src/navigation2-humble/nav2_bringup/launch/navigation_launch.py
@@ -55,6 +55,7 @@ def generate_launch_description():
     #              https://github.com/ros2/launch_ros/issues/56
     remappings = [('/tf', 'tf'),
                   ('/tf_static', 'tf_static'),
+                  ('cmd_vel', '/cmd_vel_nav'),
                   ('/cmd_vel', '/cmd_vel_nav')]
 
     # Create our own temporary YAML files that include substitutions
