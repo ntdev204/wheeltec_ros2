12d6beccf34c78dea678cc11910aed34cc0becbf
thien <thien.npc369@gmail.com>
2026-04-30 15:28:08 +0700

    feat(nav2): add cmd_vel remapping for navigation

--- src/navigation2-humble/nav2_bringup/launch/navigation_launch.py
+++ src/navigation2-humble/nav2_bringup/launch/navigation_launch.py
@@ -54,7 +54,8 @@ def generate_launch_description():
     # TODO(orduno) Substitute with `PushNodeRemapping`
     #              https://github.com/ros2/launch_ros/issues/56
     remappings = [('/tf', 'tf'),
-                  ('/tf_static', 'tf_static')]
+                  ('/tf_static', 'tf_static'),
+                  ('/cmd_vel', '/cmd_vel_nav')]
 
     # Create our own temporary YAML files that include substitutions
     # Resolve stable BT XML path: only replan when path becomes invalid
